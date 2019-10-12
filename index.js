9366a4c31c54bb2a9a163d24061382674f5e2034
Leo Kalshteyn <leo.kalshteyn@gmail.com>
2019-10-12 23:37:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,6 +4,18 @@ class Circle {
     this.radius = radius;
   }
 
+  get diameter() {
+    return `${this.name} says ${this._phrase || 'squawk'}`;
+  }
+
+  get circumference() {
+    return `${this.name} says ${this._phrase || 'squawk'}`;
+  }
+
+  get area () {
+    return `${this.name} says ${this._phrase || 'squawk'}`;
+  }
+
   set phrase(phrase) {
     this._phrase = phrase;
   }
