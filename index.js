322828f9dea0e654224498093c41b1de5c092af6
Leo Kalshteyn <leo.kalshteyn@gmail.com>
2019-10-12 23:35:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 // Add your Circle class here
-class Bird {
+class Circle {
   constructor(name) {
     this.name = name;
   }
- 
+
   set phrase(phrase) {
     this._phrase = phrase;
   }
- 
+
   get speak() {
     return `${this.name} says ${this._phrase || 'squawk'}`;
   }
